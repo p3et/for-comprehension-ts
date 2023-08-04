12c13e3bee61a7eab0062da61f9a411d7e09c5a1
André Petermann <andre.petermann@jambit.com>
2023-08-04 15:57:57 +0200

    rename V to T

--- src/for/async.ts
+++ src/for/async.ts
@@ -1,10 +1,7 @@
-import {MonadType} from "../monad/common";
-import {none, some} from "../monad/option";
-import {Monad} from "../../dist/monad/common";
+import {Monad, MonadType} from "../monad/common";
 
-
-type OptimizableFlatMap<MT extends MonadType, M extends Monad<MT, V>, I, IK extends keyof I, V> = (i: Pick<I, IK>) => Monad<MT, V> | Promise<Monad<MT, V>>
-type RegularFlatMap<MT extends MonadType, M extends Monad<MT, V>, I, V> = OptimizableFlatMap<MT, M, I, keyof I, V>
+type OptimizableFlatMap<MT extends MonadType, M extends Monad<MT, T>, I, IK extends keyof I, T> = (i: Pick<I, IK>) => Monad<MT, T> | Promise<Monad<MT, T>>
+type RegularFlatMap<MT extends MonadType, M extends Monad<MT, T>, I, T> = OptimizableFlatMap<MT, M, I, keyof I, T>
 type AsyncFlatMap<MT extends MonadType> = OptimizableFlatMap<MT, Monad<MT, any>, any, any, any>
 
 type Step<MT extends MonadType> = {
@@ -14,59 +11,59 @@ type Step<MT extends MonadType> = {
 
 type Program<MT extends MonadType, M extends Monad<MT, any>, I extends Record<string, any>> = {
     steps: Step<MT>[]
-    _<OK extends string, OV>(
-        outputKey: I extends { [_ in OK]: OV } ? never : OK,
-        fun: RegularFlatMap<MT, M, I, OV>
-    ): Program<MT, M, I & { [_ in OK]: OV }>
-    __<OK extends string, IK extends (keyof I)[], OV>(
-        outputKey: I extends { [_ in OK]: OV } ? never : OK,
+    _<OK extends string, OT>(
+        outputKey: I extends { [_ in OK]: OT } ? never : OK,
+        fun: RegularFlatMap<MT, M, I, OT>
+    ): Program<MT, M, I & { [_ in OK]: OT }>
+    __<OK extends string, IK extends (keyof I)[], OT>(
+        outputKey: I extends { [_ in OK]: OT } ? never : OK,
         inputKeys: IK,
-        fun: OptimizableFlatMap<MT, M, I, typeof inputKeys[number], OV>
-    ): Program<MT, M, I & { [_ in OK]: OV }>
-    yield<YM extends Monad<MT, V>, V>(fun: (i: I) => V): Promise<Monad<MT, V> & YM>
+        fun: OptimizableFlatMap<MT, M, I, typeof inputKeys[number], OT>
+    ): Program<MT, M, I & { [_ in OK]: OT }>
+    yield<YM extends Monad<MT, T>, T>(fun: (i: I) => T): Promise<Monad<MT, T> & YM>
 }
 
 function program<MT extends MonadType, M extends Monad<MonadType, any>, I>(steps: Step<MT>[]): Program<MT, M, I> {
     return {
         steps: steps,
-        _<OK extends string, OV>(
-            outputKey: I extends { [_ in OK]: OV } ? never : OK,
-            flatMapFunction: RegularFlatMap<MT, M, I, OV>
-        ): Program<MT, M, I & { [_ in OK]: OV }> {
+        _<OK extends string, OT>(
+            outputKey: I extends { [_ in OK]: OT } ? never : OK,
+            flatMapFunction: RegularFlatMap<MT, M, I, OT>
+        ): Program<MT, M, I & { [_ in OK]: OT }> {
             return flatMap(this, outputKey, [], flatMapFunction);
         },
-        __<OK extends string, IK extends (keyof I)[], OV>(
-            outputKey: I extends { [_ in OK]: OV } ? never : OK,
+        __<OK extends string, IK extends (keyof I)[], OT>(
+            outputKey: I extends { [_ in OK]: OT } ? never : OK,
             inputKeys: IK,
-            flatMapFunction: OptimizableFlatMap<MT, M, I, typeof inputKeys[number], OV>
-        ): Program<MT, M, I & { [_ in OK]: OV }> {
+            flatMapFunction: OptimizableFlatMap<MT, M, I, typeof inputKeys[number], OT>
+        ): Program<MT, M, I & { [_ in OK]: OT }> {
             return flatMap(this, outputKey, inputKeys, flatMapFunction);
         },
-        yield<YM extends Monad<MT, V>, V>(fun: (i: I) => V): Promise<Monad<MT, V> & YM> {
+        yield<YM extends Monad<MT, T>, T>(fun: (i: I) => T): Promise<Monad<MT, T> & YM> {
             return evaluate(this, fun);
         }
     }
 }
 
-function init<MT extends MonadType, M extends Monad<MT, V>, K extends string, V>(
+function init<MT extends MonadType, M extends Monad<MT, T>, K extends string, T>(
     key: K,
-    flatMapFunction: () => Monad<MT, V> | Promise<Monad<MT, V>>
-): Program<MT, M, { [_ in K]: V }> {
+    flatMapFunction: () => Monad<MT, T> | Promise<Monad<MT, T>>
+): Program<MT, M, { [_ in K]: T }> {
     return program([{key: key, fun: flatMapFunction}])
 }
 
-function flatMap<MT extends MonadType, M extends Monad<MT, OV>, I, K extends string, IK extends (keyof I)[], OV>(
+function flatMap<MT extends MonadType, M extends Monad<MT, OT>, I, K extends string, IK extends (keyof I)[], OT>(
     oldProgram: Program<MT, M, I>,
-    outputKey: I extends { [_ in K]: OV } ? never : K,
+    outputKey: I extends { [_ in K]: OT } ? never : K,
     inputKeys: IK,
-    flatMapFunction: OptimizableFlatMap<MT, M, I, typeof inputKeys[number], OV>
-): Program<MT, M, I & { [_ in K]: OV }> {
+    flatMapFunction: OptimizableFlatMap<MT, M, I, typeof inputKeys[number], OT>
+): Program<MT, M, I & { [_ in K]: OT }> {
     return program(oldProgram.steps.concat({key: outputKey, fun: flatMapFunction}))
 }
 
-async function evaluate<MT extends MonadType, M extends Monad<MT, V>, I, V>(
+async function evaluate<MT extends MonadType, M extends Monad<MT, T>, I, T>(
     program: Program<MT, Monad<MT, any>, I>,
-    mapFunction: (i: I) => V
+    mapFunction: (i: I) => T
 ): Promise<M> {
     const head = program.steps[0]
 
@@ -85,10 +82,10 @@ async function evaluate<MT extends MonadType, M extends Monad<MT, V>, I, V>(
 }
 
 export namespace AsyncFor {
-    export function _<MT extends MonadType, M extends Monad<MonadType, V>, K extends string, V>(
+    export function _<MT extends MonadType, M extends Monad<MonadType, T>, K extends string, T>(
         key: K,
-        flatMapFunction: () => Monad<MT, V> | Promise<Monad<MT, V>>
-    ): Program<MT, M, { [_ in K]: V }> {
+        flatMapFunction: () => Monad<MT, T> | Promise<Monad<MT, T>>
+    ): Program<MT, M, { [_ in K]: T }> {
         return init(key, flatMapFunction)
     }
 }
